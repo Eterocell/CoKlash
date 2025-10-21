e1946675db9b0cc1700aa63cf54ca414e45ed8a2
Eterocell <eterocell@outlook.com>
2025-10-21 14:41:37 +0800

    chore(deps): update dependencies
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ subprojects {
 
             minSdk = 23
             targetSdk = 36
-            buildToolsVersion = "36.0.0"
+            buildToolsVersion = "36.1.0"
 
             versionName = "3.0.0-beta05"
             versionCode = "03000050".toInt()
@@ -50,7 +50,7 @@ subprojects {
             }
         }
 
-        ndkVersion = "28.2.13676358"
+        ndkVersion = "29.0.14206865"
 
         compileSdkVersion(36)
 
